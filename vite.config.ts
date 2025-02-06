a8d3ff892915e64eb5b18f16fd5fd79e2345da10
lucarl07 <junitinho2007@gmail.com>
2025-02-06 16:29:08 -0300

    🔧 chore: Set aliases for 7 paths
    The aliases are:
      - src: ./src,
      - fonts: ./src/assets/fonts,
      - images: ./src/assets/images,
      - components: ./src/components,
      - contexts: ./src/contexts,
      - hooks: ./src/hooks,
      - utils: ./src/utils,

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      src: "/src",
+      fonts: "/src/assets/fonts",
+      images: "/src/assets/images",
+      components: "/src/components", 
+      contexts: "/src/contexts", 
+      hooks: "/src/hooks",
+      utils: "/src/utils",
+    }
+  },
 })
