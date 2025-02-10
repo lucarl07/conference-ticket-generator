3ce8aaf14247115de0630e2d72b56bcaa4997053
lucarl07 <junitinho2007@gmail.com>
2025-02-10 16:43:04 -0300

    ✨ feat: Implement React Router's routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router'
+
 import './index.css'
 import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
+const root = document.getElementById('root')
+
+createRoot(root!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
